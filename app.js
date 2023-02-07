a566be7026b679b4f05956f16ba1cc4a0d4a09bd
CarlosPrieto <carlosprieto66@icloud.com>
2023-02-07 13:13:39 -0800

    linked api url, assigned data to constants

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 const express = require("express");
 const app = express();
 const config = require("dotenv").config();
+const https = require("node:https");
 
 const port = process.env.PORT;
 const apiKey = process.env.API_KEY;
+const urlPrefix = process.env.URL_PREFIX;
 
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html");
-})
+});
 
+app.post("/", (req, res)=>{
+    const cityName = req.body.cityInput;
+   // const tempUnit = req.body.tempUnit;
+    const apiUrl = urlPrefix + cityName + apiKey;
+    
+    https.get(apiUrl, (response)=>{
+        console.log("statusCode:", response.statusCode);
+        
+        response.on("data", (data)=>{
+            const weatherApp = JSON.parse(data);
+            const currentTemp = weatherApp.main.temp;
+            const lowTemp = weatherApp.main.temp_min;
+            const highTemp = weatherApp.main.temp_max;
+            const weatherDis = weatherApp.weather[0].description;
+            const iconCode = weatherApp.weather[0].icon;
+            const weatherIcon = `<img src="http://openweathermap.org/img/wn/${iconCode}@2x.png">`;
 
+        });
+
+       
+    });
+});
 
 
 
 app.listen(port, ()=>{
     console.log("listening on port " + port);
-})
+});
